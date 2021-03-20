d68ea20c49e4c77d12f242b4108fa9023e86e490
JabJabJab <JabJabJab@github.com>
2021-03-20 10:52:43 -0500

    Documented FieldProperties.

--- core/src/main/kotlin/jab/langpack/core/objects/FieldProperties.kt
+++ core/src/main/kotlin/jab/langpack/core/objects/FieldProperties.kt
@@ -1,15 +1,18 @@
 package jab.langpack.core.objects
 
 /**
- * TODO: Document.
+ * **FieldProperties** handles information for field inputs for strings. This sanitizes flags as fields to support
+ * multiple formats without hard-coding the disassembly of one format's syntax.
  *
  * @author Jab
  *
- * @property raw
- * @property name
- * @property placeholder
- * @property resolve
- * @property packageScope
+ * @property raw The unprocessed field.
+ * @property name The name of the field.
+ * @property placeholder The placeholder to use if the field is not resolved.
+ * @property resolve If set to true, the field needs to resolve during the *walk* cycle, immediately following loading
+ * its package.
+ * @property packageScope If set to true, queries for this field will start on the package-level scope.
+ * (Non-relative lookup)
  */
 class FieldProperties(
     val raw: String,
