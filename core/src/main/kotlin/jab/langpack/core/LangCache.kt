74500d2515ba96d0c909311cab44927bdbc6b9e7
JabJabJab <JabJabJab@github.com>
2021-03-20 10:52:08 -0500

    Updated documentation for LangCache.

--- core/src/main/kotlin/jab/langpack/core/LangCache.kt
+++ core/src/main/kotlin/jab/langpack/core/LangCache.kt
@@ -6,11 +6,8 @@ import jab.langpack.core.objects.LangArg
 import java.util.*
 
 /**
- * TODO: Update documentation to reflect Definition API update.
- *
- * The **LangCache** class proxies the processed calls for [LangPack], storing them as a cached result to be
- * referred to if called again. The purpose of the cache is to avoid wasted calculations on multiple calls that returns
- * the same result.
+ * **LangCache** wraps a [LangPack] instance as type **Pack**, storing results from queries. If you need to call
+ * multiple times for the same result, use a cache.
  *
  * If dynamic calls to the same field are passed different arguments or the arguments referenced in the string
  * processing are dynamic, do not use a cache to call to them, otherwise the first call to the cache for this string
@@ -18,6 +15,7 @@ import java.util.*
  *
  * @author Jab
  *
+ * @param Pack The type of LangPack implementation.
  * @property pack The lang-pack instance to call to and cache the results.
  */
 open class LangCache<Pack : LangPack>(val pack: Pack) {
@@ -26,54 +24,6 @@ open class LangCache<Pack : LangPack>(val pack: Pack) {
     private val cacheList: EnumMap<Language, HashMap<String, List<String?>>> = EnumMap(Language::class.java)
 
     override fun toString(): String = "LangCache(pack=$pack, cache=$cache, cacheList=$cacheList)"
-
-    /**
-     * @see LangPack.getString
-     */
-    fun getString(query: String, lang: Language = pack.defaultLang, vararg args: LangArg): String {
-
-        val fieldLower = query.toLowerCase()
-
-        if (cache.containsKey(lang)) {
-            val cache = cache[lang]!!
-            if (cache.containsKey(fieldLower)) {
-                return cache[fieldLower]!!
-            }
-        }
-
-        var value = pack.getString(query, lang, *args)
-        if (value == null) {
-            value = query.toLowerCase()
-        }
-
-        val cache = cache.computeIfAbsent(lang) { HashMap() }
-        cache[fieldLower] = value
-
-        return value
-    }
-
-    /**
-     * @see LangPack.getList
-     */
-    fun getList(query: String, lang: Language = pack.defaultLang, vararg args: LangArg): List<String?>? {
-
-        val fieldLower = query.toLowerCase()
-
-        if (cacheList.containsKey(lang)) {
-            val cacheList = cacheList[lang]!!
-            if (cacheList.containsKey(fieldLower)) {
-                return cacheList[fieldLower]
-            }
-        }
-
-        val value = pack.getList(query, lang, *args)
-        if (value != null) {
-            val cacheList = cacheList.computeIfAbsent(lang) { HashMap() }
-            cacheList[fieldLower] = value
-        }
-
-        return value
-    }
 
     /**
      * Clears results stored in the cache. If no arguments are provided, the entire language section of the cache is
@@ -103,5 +53,51 @@ open class LangCache<Pack : LangPack>(val pack: Pack) {
     fun clear() {
         cache.clear()
         cacheList.clear()
+    }
+
+    /**
+     * @see LangPack.getString
+     */
+    fun getString(query: String, lang: Language = pack.defaultLang, vararg args: LangArg): String {
+
+        val fieldLower = query.toLowerCase()
+
+        if (cache.containsKey(lang)) {
+            val cache = cache[lang]!!
+            if (cache.containsKey(fieldLower)) {
+                return cache[fieldLower]!!
+            }
+        }
+
+        var value = pack.getString(query, lang, *args)
+        if (value == null) value = query.toLowerCase()
+
+        val cache = cache.computeIfAbsent(lang) { HashMap() }
+        cache[fieldLower] = value
+
+        return value
+    }
+
+    /**
+     * @see LangPack.getList
+     */
+    fun getList(query: String, lang: Language = pack.defaultLang, vararg args: LangArg): List<String?>? {
+
+        val fieldLower = query.toLowerCase()
+
+        if (cacheList.containsKey(lang)) {
+            val cacheList = cacheList[lang]!!
+            if (cacheList.containsKey(fieldLower)) {
+                return cacheList[fieldLower]
+            }
+        }
+
+        val value = pack.getList(query, lang, *args)
+        if (value != null) {
+            val cacheList = cacheList.computeIfAbsent(lang) { HashMap() }
+            cacheList[fieldLower] = value
+        }
+
+        return value
     }
 }
