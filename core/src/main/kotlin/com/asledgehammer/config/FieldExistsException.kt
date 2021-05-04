927e7d86779f4535f282d1b809a0873de528f8a1
JabJabJab <JabJabJab@github.com>
2021-05-03 21:43:25 -0500

    Created FieldExistsException.

---
+++ core/src/main/kotlin/com/asledgehammer/config/FieldExistsException.kt
@@ -0,0 +1,10 @@
+package com.asledgehammer.config
+
+/**
+ * **FieldAlreadyExistsException** TODO: Document.
+ *
+ * @author Jab
+ *
+ * @param msg The message to display when thrown.
+ */
+class FieldExistsException(msg: String) : RuntimeException(msg)
